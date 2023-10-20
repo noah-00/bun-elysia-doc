725ec5951ca91bbd2c7c65f5f08e55ac7867166f
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-20 20:21:38 +0900

    ✨ get route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysiaaa").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysiaaa")
+  .get("/post/:id", ({ params }) => `Post ID: ${params.id}`)
+  .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
