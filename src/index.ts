89754ac4f9a5f693979da3dea49034e9320df9c1
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-20 20:42:31 +0900

    Route and Handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .get("/", () => "Hello Elysiaaa")
+  .get("/", () => "Hello Elysia")
   .get("/post/:id", ({ params }) => `Post ID: ${params.id}`)
+  .post("/post", ({ body, set }) => {
+    set.status = 201;
+    return body;
+  })
+  .get("/track/*", () => {
+    return "Hello Track";
+  })
+  .get("/tracks", () => {
+    return new Response(
+      JSON.stringify({
+        tracks: ["track1", "track2"],
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  })
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
